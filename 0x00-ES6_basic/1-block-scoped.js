224bc85f324a395ef2646370afb2e4964fcd647f
salahSurvivor <salaheddinenaji2000@gmail.com>
2024-04-23 21:31:14 +0000

    TASK 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,13 +1,13 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    task = true;
-    task2 = false;
+    const task = true;
+    const task2 = false;
 
-    task ? task = false : task = false;
-    !task2 ? task2 = true : task2 = true;
+  
+    return [!task, !task2];
   }
 
   return [task, task2];
