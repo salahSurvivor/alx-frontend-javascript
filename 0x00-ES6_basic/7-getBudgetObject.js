76e7d7301fee2d05ef402197a98684061469e2ef
salahSurvivor <salaheddinenaji2000@gmail.com>
2024-04-24 20:23:42 +0000

    TASK 7

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,9 +1,14 @@
-export default function getBudgetObject(income, gdp, capita) {
-	    const budget = {
-		          income,
-		          gdp,
-		          capita,
-		        };
-	  
-	    return budget;
+function getCurrentYear() {
+  const date = new Date();
+  return date.getFullYear();
+}
+
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  const budget = {
+    [`income-${getCurrentYear()}`]: income,
+    [`gdp-${getCurrentYear()}`]: gdp,
+    [`capita-${getCurrentYear()}`]: capita,
+  };
+
+  return budget;
 }
